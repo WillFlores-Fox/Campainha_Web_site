48079c2b6b2a1411f0fbd39d1eba580508e21855
Will Afton <70648524+WillFlores-Fox@users.noreply.github.com>
2025-07-07 17:30:25 -0300

    fix js

--- js/card.js
+++ js/card.js
@@ -10,15 +10,34 @@ function atualizarCard() {
     ultimaMensagem = data;
 
     document.getElementById("notificacao").innerHTML = `
-          <div class="card">
-          <h1>Esta tocando sua campainha!</h1>
-            <img src="${imagem}" class="photo" />
-            <h3>${nome}</h3>
-            <p>${mensagem}</p>
-          </div>
-        `;
+      <div class="card">
+        <h1>Está tocando sua campainha!</h1>
+        <img src="${imagem}" class="photo" />
+        <h3>${nome}</h3>
+        <p>${mensagem}</p>
+        <div class="botoes">
+          <button onclick="atenderVisitante()">Atender</button>
+          <button onclick="recusarVisitante()">Recusar</button>
+          <button onclick="estouOcupado()">Estou Ocupado</button>
+        </div>
+      </div>
+    `;
   }
 }
 
-// Verifica a cada 1 segundo
+function atenderVisitante() {
+  alert("Você atendeu o visitante.");
+  document.getElementById("notificacao").innerHTML = "";
+}
+
+function recusarVisitante() {
+  alert("Você recusou o visitante.");
+  document.getElementById("notificacao").innerHTML = "";
+}
+
+function estouOcupado() {
+  alert("Você informou que está ocupado.");
+  document.getElementById("notificacao").innerHTML = "";
+}
+
 setInterval(atualizarCard, 1000);
